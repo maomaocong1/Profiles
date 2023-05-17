77e8b0949d73295af41c22f5958bac5826edd087
maomaocong1 <maomaocong1@163.com>
2023-05-17 09:51:40 -0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <><div>
-      <a id = "resume" className="border-solid border-2 p-2 rounded-md" href='mailto:maomaocong1@gmail.com'>Contact Me</a>
+      <a id = "resume" className="my-10 border-solid border-2 p-2 rounded-md" href='mailto:maomaocong1@gmail.com'>Contact Me</a>
     </div>
     </>
   )

fcc6371af56a5e308471f72fc3e29d5e1684f0a3
maomaocong1 <maomaocong1@163.com>
2023-05-17 10:00:28 -0700

    modify margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <><div>
-      <a id = "resume" className="my-10 border-solid border-2 p-2 rounded-md" href='mailto:maomaocong1@gmail.com'>Contact Me</a>
+    <><div className="my-5">
+      <a id = "resume" className=" border-solid border-2 p-2 rounded-md" href='mailto:maomaocong1@gmail.com'>Contact Me</a>
     </div>
     </>
   )
